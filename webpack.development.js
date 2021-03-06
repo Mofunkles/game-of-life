47a72c273258d4c95a18f0c67c4ded5bad3ee509
Mofunkles <danseddon@hotmail.co.uk>
2021-03-06 18:34:53 +0000

    Refactored to Webpack bundling

---
+++ webpack.development.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    compress: true,
+    open: true,
+    port: 4000,
+    watchOptions: {
+      ignore: /node_modules/,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
